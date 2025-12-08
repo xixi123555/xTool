4a6c554821fb5ac6b7e92aa298df0df9f3c9c5e8
tangxixi <tangxixi@udesk.cn>
2025-12-08 20:02:21 +0800

    fix: 启动脚本更改

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps : [{
     name: 'xTool-server',
     script: 'npm',           // 使用 npm
-    args: 'run start',       // npm 参数
-    cwd: '/root/.nvm/xTool/server',  // 项目路径
+    args: 'run start:server',       // npm 参数
+    cwd: '/root/.nvm/xTool',  // 项目路径
     instances: 1,            // 实例数（1 或 max）
     autorestart: true,       // 自动重启
     watch: false,            // 是否监听文件变化
